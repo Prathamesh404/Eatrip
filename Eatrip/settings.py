677ba1c1ed88c1dda6bcbb7d3dd99b74cb9564fe
Prathamesh <encrypt@Prathameshs-MacBook-Air.local>
2017-11-28 22:23:06 +0530

    Facebook authentication p1

--- Eatrip/settings.py
+++ Eatrip/settings.py
@@ -153,3 +153,15 @@ SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
 SOCIAL_AUTH_FACEBOOK_PROFILE_EXTRA_PARAMS = {
     'fields': 'id, name, email'
 }
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'EatripApp.social_auth_pipeline.create_user_by_type',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)
--- EatripApp/admin.py
+++ EatripApp/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from EatripApp.models import Restro
+from EatripApp.models import Restro, Customer, Driver
 
 admin.site.register(Restro)
+admin.site.register(Customer)
+admin.site.register(Driver)
